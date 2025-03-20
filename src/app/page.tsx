354cde5af8c3d4f94653c0a5516ade6934178fe4
Vlashex <rmaynavine@gmail.com>
2025-03-20 16:26:38 +0300

    Удалил случайно попавшее в коммит

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,8 @@
 import DnDContainer from "@/widgets/DnDContainer";
-import { useCookies } from "react-cookie";
-
-
-
-
 
 
 export default function Home() {
 
-  const [cookie, setCookie] = useCookies(["access_token", "refresh_token"])
 
   return (
     <>
